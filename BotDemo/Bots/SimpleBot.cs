2507c1061859e78544ec1340d958c12930919b3f
Michael Aiello <michael.aiello@team.seven2.com>
2019-09-11 14:11:53 -0700

    Add project files.

---
+++ BotDemo/Bots/SimpleBot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BotDemo.Bots
+{
+    public class SimpleBot : IBot
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            if(turnContext.Activity.Type is ActivityTypes.Message)
+            {
+                string input = turnContext.Activity.Text;
+                await turnContext.SendActivityAsync($"SimpleBot: {input}");
+            }
+        }
+    }
+}
